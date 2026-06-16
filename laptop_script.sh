099cba1276f8f26cfdcb4b28d6909ddc41508383
unknown <sakshigawale2112@gmail.com>
2026-06-16 10:38:32 +0530

    Add laptop script

---
+++ laptop_script.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+yum update -y
+yum install nginx -y
+systemctl start nginx 
+systemctl enable nginx 
+sudo rm -rf /usr/share/nginx/html/*
+mkdir -p /usr/share/nginx/html/laptop
+echo "<h1>welcome to laptop store</h1>" > /usr/share/nginx/html/laptop/index.html
+echo "<h1>welcome to cloudblitz</h1>" > /usr/share/nginx/html/index.html
+systemctl restart nginx
